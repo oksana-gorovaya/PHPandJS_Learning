a3d0c0605f98614333a9af73297182c84981bcde
oksana-gorovaya <ksenia15grv@gmail.com>
2019-06-18 21:51:03 +0300

    Redundant variable removed.

--- filesChain.php
+++ filesChain.php
@@ -3,12 +3,10 @@ function checkFiles()
 {
     $fileName = '699991.txt';
     while (preg_match('(^[0-9]+[.]txt$)', $fileName)){
-        $path = file_get_contents('https://stepic.org/media/attachments/course67/3.6.3/' . $fileName);
-        $fileName = $path;
+        $fileName = file_get_contents('https://stepic.org/media/attachments/course67/3.6.3/' . $fileName);
     }
 
     return $fileName;
 }
-
 
 echo(checkFiles());
